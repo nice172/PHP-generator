c6b44b0853da8ba12fa6583e0451e6efff76c64c
zhangming <zm7413299@163.com>
2017-07-18 15:48:34 +0800

    task

---
+++ task.php
@@ -0,0 +1,38 @@
+<?php
+class Task{
+
+	protected $TaskId;
+	protected $coroutine;
+	protected $sendValue = null;
+	protected $beforeFirstYied = true;
+
+
+
+	public function __construct($TaskId, Generator $coroutine){
+		$this->taskId = $taskId;
+		$this->coroutine = $coroutine;
+	}
+
+	public function getTaskId(){
+		return $this->taskId;
+	}
+
+	public function setSendValue(){
+		$this->sendValue = $sendValue;
+	}
+
+	public function run(){
+		if ($this->beforeFirstYied) {
+			$this->beforeFirstYied = false;
+			return $this->coroutine->current();
+		}else{
+			$ret= $this->coroutine->send($this->sendValue);
+			$this->sendValue = null;
+			return $ret;
+		}
+	}
+
+	public function finished(){
+		return !this->coroutine->valid();
+	}
+}
