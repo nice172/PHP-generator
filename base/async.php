3b045e0b54905a1ca8d321a4f49bfdb577a6590a
silen <zm7413299@163.com>
2017-07-19 23:28:42 +0800

    async

---
+++ base/async.php
@@ -0,0 +1,14 @@
+<?php
+
+interface Async{
+
+    /**
+     * 开启异步任务，完成是执行回调，任务结果或异常通过回调参数传递
+     * @param callable $callback
+     *      continuation :: (mixed $result = null, \Exception|null $ex = null)
+     * @return void 
+     */
+    public function begin(){
+        
+    }
+}
