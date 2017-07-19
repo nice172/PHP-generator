740c1533a3e53479bcf20ba75979ed59ec4836c1
silen <zm7413299@163.com>
2017-07-20 00:01:48 +0800

    init interface

---
+++ base/gen.php
@@ -0,0 +1,37 @@
+<?php
+
+   /**
+     * 统一生成器接口 current,key,next,rewind,valid
+     */
+   class Gen
+   {
+
+       public $isfirst = true;
+       public $generator;
+
+
+       function __construct($generator)
+       {
+           $this->generator = $generator;
+       }
+
+    /**
+     * 检查当前标量是否有效
+     */
+       public function valid(){
+            return $this->generator->valid();
+       }
+
+       /*
+        *generator implements Iterator
+        */
+        public function send($value = null){
+            if($this->isfirst){
+                $this->isfirst = true;
+                return $this->generator->current();
+            }else{
+                return $this->generator->send($value)
+            }
+        }
+
+   }
