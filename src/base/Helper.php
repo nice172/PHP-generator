cd7712b727be3a6c989fd1aaf083d8d91579f1ed
silen <zm7413299@163.com>
2017-07-27 23:15:45 +0800

    timeout

---
+++ src/base/Helper.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 2017/7/27
+ * Time: 23:13
+ */
+
+
+/**
+ * timeout 超时处理
+ */
+class Helper implements Async
+{
+    public $parent;
+    public $tasks;
+    public $continuation;
+    public $done;
+
+    public function __construct(array $tasks, AsyncTask $parent = null)
+    {
+        assert(!empty($tasks));
+        $this->tasks = $tasks;
+        $this->parent = $parent;
+        $this->done = false;
+    }
+
+    public function begin(callable $continuation)
+    {
+        $this->continuation = $continuation;
+        foreach ($this->tasks as $id => $task) {
+            (new AsyncTask($task, $this->parent))->begin($this->continuation($id));
+        };
+    }
+
+    private function continuation($id)
+    {
+        return function($r, $e = null) use($id) {
+            if ($this->done) {
+                return;
+            }
+            $this->done = true;
+
+            if ($this->continuation) {
+                $k = $this->continuation;
+                $k($r, $e);
+            }
+        };
+    }
+}
