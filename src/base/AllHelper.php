f389b27ce93cda2695b3907899c40ba6b3449b53
silen <zm7413299@163.com>
2017-07-30 18:48:50 +0800

    continuation

--- src/base/AllHelper.php
+++ src/base/AllHelper.php
@@ -21,10 +21,44 @@ class AllHelper implements Async{
 
     public function __construct(array $tasks, AsyncTask $parent = null)
     {
-
+        $this->tasks = $tasks;
+        $this->parent = $parent;
+        $this->n = count($tasks);
+        assert($this->n > 0);
+        $this->results = [];
     }
 
     public function begin(callable $continuation = null){
+        $this->continuation = $continuation;
+        foreach ($this->tasks as $id => $task) {
+            (new AsyncTask($task, $this->parent))->begin($this->continuation($id));
+        };
+    }
 
+    private function continuation($id)
+    {
+        return function($r, $e = null) use($id) {
+            if ($this->done) {
+                return;
+            }
+
+            // 任一回调发生异常，终止任务
+            if ($e) {
+                $this->done = true;
+                $k = $this->continuation;
+                $k(null, $e);
+                return;
+            }
+
+            $this->results[$id] = $r;
+            if (--$this->n === 0) {
+                // 所有回调完成，终止任务
+                $this->done = true;
+                if ($this->continuation) {
+                    $k = $this->continuation;
+                    $k($this->results);
+                }
+            }
+        };
     }
 }
